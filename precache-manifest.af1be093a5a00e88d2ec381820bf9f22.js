aebb4b73b7b60c962ce277da4a5e7639850517ce
nicobuffa <nicobuffa94@gmail.com>
2020-04-28 00:08:54 -0300

    Updates

---
+++ precache-manifest.af1be093a5a00e88d2ec381820bf9f22.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb859f442b3940564467b4f15e704622",
+    "url": "/catalogo/index.html"
+  },
+  {
+    "revision": "7d5f6314d596c5e73606",
+    "url": "/catalogo/static/css/2.dbb97054.chunk.css"
+  },
+  {
+    "revision": "b04d91089f372cdfa0d2",
+    "url": "/catalogo/static/css/main.3479ca8a.chunk.css"
+  },
+  {
+    "revision": "7d5f6314d596c5e73606",
+    "url": "/catalogo/static/js/2.99ff7969.chunk.js"
+  },
+  {
+    "revision": "b04d91089f372cdfa0d2",
+    "url": "/catalogo/static/js/main.65d6ae88.chunk.js"
+  },
+  {
+    "revision": "c1a51f7fbaacdaba7a67",
+    "url": "/catalogo/static/js/runtime~main.f7b335b1.js"
+  },
+  {
+    "revision": "b9b601d7e7aa7990768c282c6d0d69ce",
+    "url": "/catalogo/static/media/BelgranoTitA.b9b601d7.jpg"
+  },
+  {
+    "revision": "542c1aadebac229fefacef2a7931077d",
+    "url": "/catalogo/static/media/BocaAltA.542c1aad.jpg"
+  },
+  {
+    "revision": "6b82cd5637c4513f0f6de68e94840d12",
+    "url": "/catalogo/static/media/BocaTitA.6b82cd56.jpg"
+  },
+  {
+    "revision": "6b82cd5637c4513f0f6de68e94840d12",
+    "url": "/catalogo/static/media/BocaTitN.6b82cd56.jpg"
+  },
+  {
+    "revision": "3b5e074b55258bbc03dd2adc70fe90db",
+    "url": "/catalogo/static/media/Camiseta.3b5e074b.jpg"
+  },
+  {
+    "revision": "bdbfc837618dd0aa5c6446867ddfffca",
+    "url": "/catalogo/static/media/Camisetas.bdbfc837.jpg"
+  },
+  {
+    "revision": "69c5e98aba20ff961ee9f808f4165ddf",
+    "url": "/catalogo/static/media/Est.69c5e98a.jpg"
+  },
+  {
+    "revision": "dde41ff06f19a2bd75566cd8fb43ac0c",
+    "url": "/catalogo/static/media/Oferta.dde41ff0.png"
+  },
+  {
+    "revision": "3449cc7405d08e0262510d12c3c986e8",
+    "url": "/catalogo/static/media/Ofertas.3449cc74.png"
+  },
+  {
+    "revision": "2faaf85dd31d8920be315bc7f6aed18b",
+    "url": "/catalogo/static/media/Pantalon.2faaf85d.jpg"
+  },
+  {
+    "revision": "27060a329a2bc9fa56d06fb269001b05",
+    "url": "/catalogo/static/media/Pantalones.27060a32.jpg"
+  },
+  {
+    "revision": "1053d57afdf9afb589692960ac8dab67",
+    "url": "/catalogo/static/media/RiverAltA.1053d57a.jpg"
+  },
+  {
+    "revision": "51458ac20a0562e0da24a09a041930d5",
+    "url": "/catalogo/static/media/RiverArqA.51458ac2.jpg"
+  },
+  {
+    "revision": "2411831bcaf860d181858cf0b53f04eb",
+    "url": "/catalogo/static/media/RiverTitA.2411831b.jpg"
+  },
+  {
+    "revision": "ad4794f1acda60fd4daaecb352d3ceed",
+    "url": "/catalogo/static/media/Short.ad4794f1.jpg"
+  },
+  {
+    "revision": "76379e40d7997cd001f16bbd4ec4978c",
+    "url": "/catalogo/static/media/Shorts.76379e40.jpg"
+  },
+  {
+    "revision": "a6c20c08cebea7447f463737cabae1ad",
+    "url": "/catalogo/static/media/TalleCamisetaAdulto.a6c20c08.jpg"
+  },
+  {
+    "revision": "d00da45acc2004576fe47e237c48f050",
+    "url": "/catalogo/static/media/no-disp.d00da45a.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/catalogo/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/catalogo/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/catalogo/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/catalogo/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "5333b8d98cfc068214effba3847bcec8",
+    "url": "/catalogo/static/media/sf-sports-night-webfont.5333b8d9.woff2"
+  },
+  {
+    "revision": "fd9dd20fa6b928ba1a63f140176cc00b",
+    "url": "/catalogo/static/media/sf-sports-night-webfont.fd9dd20f.woff"
+  },
+  {
+    "revision": "b873fdf759633f320e9b9500815a8a04",
+    "url": "/catalogo/static/media/sf-sports-night.b873fdf7.ttf"
+  }
+]);
