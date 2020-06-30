bf35a97353261f04ce603cdc1158ed6419df1d21
nicobuffa <nicobuffa94@gmail.com>
2020-06-30 13:57:36 -0300

    cors-anywhere

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const SERVER = "https://corsanywhere.herokuapp.com/http://fanaticos-catalog-node-fanaticos-dev.apps.ca-central-1.starter.openshift-online.com/";
+export const SERVER = "https://cors-anywhere.herokuapp.com/http://fanaticos-catalog-node-fanaticos-dev.apps.ca-central-1.starter.openshift-online.com/";
 
 //export const SERVER = "http://localhost:8080/";
