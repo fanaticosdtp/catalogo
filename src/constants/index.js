441d25e9971e729cfe3ce52723fe0dcbd7c530e7
nicobuffa <nicobuffa94@gmail.com>
2020-10-27 16:21:51 -0300

    Consume Heroku

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const SERVER = "https://cors-anywhere.herokuapp.com/http://fanaticos-catalog-fanaticos-dev.apps.us-east-2.starter.openshift-online.com/";
+export const SERVER = "http://fanaticos-catalog.herokuapp.com/";
 
 //export const SERVER = "http://localhost:8080/";
